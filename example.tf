0e7d1918b359e0f143ec097c0d152bace2ba50fa
Ian Shaw <ian.shaw1991@gmail.com>
2019-03-23 19:16:11 +0000

    Added subnet and some placeholder resources.

--- example.tf
+++ example.tf
@@ -6,3 +6,31 @@ resource "aws_instance" "example" {
   ami           = "ami-2757f631"
   instance_type = "t2.micro"
 }
+
+resource "aws_subnet" "main" {
+  vpc_id = "${aws_vpc.main.id}"
+  cidr_block = "10.0.1.0/24"
+  tags = {
+    Name = "Main"
+  }
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+#resource "aws_eks_cluster" "example" {
+#  name = "example"
+#  role_arn = "${aws_iam_role.example.arn}"
+#  vpc_config {
+#     subnet_ids = ["${aws_subnet.main.id}"]
+#   }
+#}
+#
+#output "endpoint" {
+#  value = "${aws_eks_cluster.example.endpoint}"
+#}
+#
+#output "kubeconfig-certificate-authority-data" {
+#  value = "${aws_eks_cluster.example..certificate_authority.0.data}"
+#}
